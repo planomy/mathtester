88246e3e3b9d85b9933cedd014cbaa4b2ecd523d
planomy <nic@eq.edu.au>
2026-09-14 11:49:14 +1000

    Load question card shading styles

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import App from './App'
 import './index.css'
 import './testpro-makeover.css'
 import './student-test.css'
+import './editor-question-shading.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
