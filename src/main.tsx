3610617f68f44a56ce5356b6b37a391b4b66bdbe
planomy <nic@eq.edu.au>
2026-09-14 11:41:18 +1000

    Load student test workspace styles

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App'
 import './index.css'
 import './testpro-makeover.css'
+import './student-test.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
